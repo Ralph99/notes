238f56130e4968f99b5a72a58a0b94f18aaf2ce6
ralph99 <shoyomboraphaeld@gmail.com>
2021-10-29 16:28:35 +0100

    created the model and migrated

---
+++ note/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Note(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.body
